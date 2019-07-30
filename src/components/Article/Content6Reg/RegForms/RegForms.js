c1fc1b5c69daa542fe6808264136f1524765d118
WinZip <wydo@protonmail.com>
2019-07-30 22:49:54 +0300

    test

--- src/components/Article/Content6Reg/RegForms/RegForms.js
+++ src/components/Article/Content6Reg/RegForms/RegForms.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 import stl from './RegForms.module.css';
 import stlMedia from './RegFormsMedia.module.css';
 import {API} from "../../../../API/api";
@@ -14,85 +14,104 @@ const phoneMask = createTextMask({
 });
 
 
-// const file_upload = ({ input, type, meta: { touched, error, warning } } ) => {
-//     debugger
-//     delete input.value;
-//     return (
-//         <input id="photo" className={`${stl.inputFile} ${stlMedia.inputFile}`} {...input} type={type}/>
-//     )
-// };
+class RegForms extends Component {
+    constructor(props) {
+        super(props);
+        this.fileInput = React.createRef();
+    }
 
-const file_upload = ({ input, type, meta: { touched, error, warning }, updatePhoto } ) => {
 
+    render() {
+
+
+        let singUp = {
+            data: this.props.newUser,
+            setIsVisible: this.props.setIsVisible,
+            clearInfoUser: this.props.clearInfoUser,
+            resetList: this.props.resetList,
+            getUsers: this.props.getUsers
+        };
+debugger
+        
+        return (
+            <div>
+                <form onSubmit={this.props.handleSubmit} className={`${stl.forms} ${stlMedia.forms}`}>
+                    <fieldset className={this.props.newUser.validations.nameValid ? "" : stl.formsError}>
+                        <legend className={`${stl.Legend} ${stlMedia.Legend}`}>Name</legend>
+                        <Field name={'name'} placeholder="Your name" id='signUp' component="input" type="text"/>
+                        {/*<input onChange={props.updateName} type="text" placeholder="Your name" value={props.newUser.name}/>*/}
+                        <LegendErrors
+                            formErrors={this.props.newUser.validations.nameValid ? "" : this.props.newUser.validations.formErrors.name}/>
+                    </fieldset>
+                    <fieldset className={this.props.newUser.validations.emailValid ? "" : stl.formsError}>
+                        <legend className={`${stl.Legend} ${stlMedia.Legend}`}>Email</legend>
+                        <Field name={'email'} placeholder="Your email" component="input" type="text"/>
+                        {/*<input onChange={props.updateEmail} type="text" placeholder="Your email" value={props.newUser.email} />*/}
+                        <LegendErrors
+                            formErrors={this.props.newUser.validations.emailValid ? "" : this.props.newUser.validations.formErrors.email}/>
+                    </fieldset>
+                    <fieldset className={this.props.newUser.validations.phoneValid ? "" : stl.formsError}>
+                        <legend className={`${stl.Legend} ${stlMedia.Legend}`}>Phone</legend>
+                        <Field name={'phone'} placeholder="+380(__) ___ __ __" component="input"
+                               type="tel" {...phoneMask}/>
+                        {/*<InputMask onChange={props.updatePhone} type="text" mask='+380(99) 999 99 99'  maskChar="_" placeholder="+380(__) ___ __ __" value={props.newUser.phone} />*/}
+                        <LegendErrors
+                            formErrors={this.props.newUser.validations.phoneValid ? "" : this.props.newUser.validations.formErrors.phone}/>
+                    </fieldset>
+                    <fieldset className={`${stl.positionShell} ${stlMedia.positionShell}`}>
+                        <Field name={'position'} component="select" className={`${stl.position} ${stlMedia.position}`}>
+                            <option> Select your position</option>
+                            {this.props.newUser.position_data.positions.map((p) => <option key={p.id}
+                                                                                           value={p.id}> {p.name} </option>)}
+                        </Field>
+                    </fieldset>
+
+                    <fieldset className={`${stl.photo} ${stlMedia.photo}`}>
+                        {/*<input onChange={props.updatePhoto} type="file" className={`${stl.inputFile} ${stlMedia.inputFile}`} name="file" id="file" accept="image/jpeg"*/}
+                        {/*       data-multiple-caption="{count} files selected"/>*/}
+                        <Field fileInput={this.fileInput} name="photo" updatePhoto={this.props.updatePhoto} accept="image/jpeg"
+                               data-multiple-caption="{count} files selected" component={file_upload} type="file"/>
+                        <label htmlFor="photo">
+                            {/*{props.form.getUser.value? <span className={`${stl.photoName} ${stlMedia.photoName}`}>{props.form.getUser.value.photo.FileList[0].name}</span> : <span/>}*/}
+                            <strong className={`${stl.buttonUpload} ${stlMedia.buttonUpload}`}><span
+                                className={`${stl.uploadText} ${stlMedia.uploadText}`}>Upload </span> <span
+                                className={`${stl.uploadIcon} ${stlMedia.uploadIcon}`}>{upload}</span> </strong>
+                        </label>
+                        <LegendErrors
+                            formErrors={this.props.newUser.validations.photoValid ? "" : this.props.newUser.validations.formErrors.photo}/>
+                        <p className={`${stl.commentPhoto} ${stlMedia.commentPhoto}`}> File format jpg up to 5 MB, the
+                            minimum size of
+                            70x70px</p>
+                    </fieldset>
+                    <button>test button</button>
+                    <div>{this.fileInput.current === null ? "1" : "3"}</div>
+                </form>
+                <div className={`${stl.buttonSell} ${stlMedia.buttonSell}`}>
+                    {regUsersValidation(this.props)
+                        ? <div className="buttonPrimary" onClick={() => {
+                            API.postUsers((singUp))
+                        }}>Sign Up</div> : <div className="buttonInactiv">Sign Up</div>}
+                </div>
+            </div>
+        )
+    }
+}
+
+
+
+const file_upload = ({ fileInput,input, type, meta: { touched, error, warning }, updatePhoto } ) => {
+debugger
+    // let fileName = React.createRef();
     delete input.value;
 
     // updatePhoto(input);
     return (
-                <input id="photo"  className={`${stl.inputFile} ${stlMedia.inputFile}`} {...input} type={type}/>
+<div>
+    <input id="photo" ref={fileInput} className={`${stl.inputFile} ${stlMedia.inputFile}`} {...input} type={type}/>
+</div>
     )
 };
 
-const RegForms = (props) => {
-
-let singUp = {
-    data: props.newUser,
-    setIsVisible: props.setIsVisible,
-    clearInfoUser: props.clearInfoUser,
-    resetList: props.resetList,
-    getUsers: props.getUsers
-};
-
-
-    return (
-        <div>
-            <form onSubmit={props.handleSubmit} className={`${stl.forms} ${stlMedia.forms}`}>
-                <fieldset className={props.newUser.validations.nameValid ? "" : stl.formsError}>
-                    <legend className={`${stl.Legend} ${stlMedia.Legend}`}>Name</legend>
-                    <Field name={'name'} placeholder="Your name" id='signUp' component="input" type="text" />
-                    {/*<input onChange={props.updateName} type="text" placeholder="Your name" value={props.newUser.name}/>*/}
-                    <LegendErrors formErrors={props.newUser.validations.nameValid ? "" : props.newUser.validations.formErrors.name}/>
-                </fieldset>
-                <fieldset className={props.newUser.validations.emailValid ? "" : stl.formsError}>
-                    <legend className={`${stl.Legend} ${stlMedia.Legend}`}>Email</legend>
-                    <Field name={'email'} placeholder="Your email" component="input" type="text" />
-                    {/*<input onChange={props.updateEmail} type="text" placeholder="Your email" value={props.newUser.email} />*/}
-                    <LegendErrors formErrors={props.newUser.validations.emailValid ? "" : props.newUser.validations.formErrors.email}/>
-                </fieldset>
-                <fieldset className={props.newUser.validations.phoneValid ? "" : stl.formsError}>
-                    <legend className={`${stl.Legend} ${stlMedia.Legend}`}>Phone</legend>
-                    <Field name={'phone'} placeholder="+380(__) ___ __ __" component="input"  type="tel" {...phoneMask}/>
-                    {/*<InputMask onChange={props.updatePhone} type="text" mask='+380(99) 999 99 99'  maskChar="_" placeholder="+380(__) ___ __ __" value={props.newUser.phone} />*/}
-                    <LegendErrors formErrors={props.newUser.validations.phoneValid ? "" : props.newUser.validations.formErrors.phone}/>
-                </fieldset>
-                <fieldset className={`${stl.positionShell} ${stlMedia.positionShell}`}>
-                    <Field name={'position'} component="select" className={`${stl.position} ${stlMedia.position}`}>
-                        <option> Select your position </option>
-                        {props.newUser.position_data.positions.map((p) => <option key={p.id} value={p.id}> {p.name} </option>) }
-                    </Field>
-                </fieldset>
-
-                <fieldset className={`${stl.photo} ${stlMedia.photo}`}>
-                    {/*<input onChange={props.updatePhoto} type="file" className={`${stl.inputFile} ${stlMedia.inputFile}`} name="file" id="file" accept="image/jpeg"*/}
-                    {/*       data-multiple-caption="{count} files selected"/>*/}
-                    <Field name="photo" updatePhoto={props.updatePhoto} accept="image/jpeg" data-multiple-caption="{count} files selected" component={file_upload} type="file" />
-                    <label htmlFor="photo">
-                        {/*{props.form.getUser.value? <span className={`${stl.photoName} ${stlMedia.photoName}`}>{props.form.getUser.value.photo.FileList[0].name}</span> : <span/>}*/}
-                        <strong className={`${stl.buttonUpload} ${stlMedia.buttonUpload}`}><span className={`${stl.uploadText} ${stlMedia.uploadText}`}>Upload </span> <span className={`${stl.uploadIcon} ${stlMedia.uploadIcon}`}>{upload}</span> </strong>
-                    </label>
-                    <LegendErrors formErrors={props.newUser.validations.photoValid ? "" : props.newUser.validations.formErrors.photo}/>
-                    <p className={`${stl.commentPhoto} ${stlMedia.commentPhoto}`}> File format jpg up to 5 MB, the minimum size of
-                        70x70px</p>
-                </fieldset>
-
-                <button>test button</button>
-            </form>
-            <div className={`${stl.buttonSell} ${stlMedia.buttonSell}`}>
-                {regUsersValidation(props)
-                    ? <div className="buttonPrimary" onClick={()=> {API.postUsers((singUp))}}>Sign Up</div> : <div className="buttonInactiv" >Sign Up</div>}
-            </div>
-        </div>
-    )
-};
 
 const RegFormsRedux = reduxForm({
     // a unique name for the form
